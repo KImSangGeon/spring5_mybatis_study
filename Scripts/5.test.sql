8ea0cd5eebc7848ee150cbfa5f8f7eb2c2778e13
KImSangGeon <tkdrjs7@naver.com>
2021-05-12 10:38:18 +0900

    1 vs n 매핑 참고

--- Scripts/5.test.sql
+++ Scripts/5.test.sql
@@ -22,3 +22,14 @@ delete from students  where stud_id =3;
 select stud_id, name, email, phone, dob, a.addr_id, street, city, state, zip, country
 	from students s  join addresses a  on s.addr_id =a.addr_id 
 	where stud_id =1;
+	
+select t.tutor_id, t.name as tutor_name, email, c.course_id, c.name, description, start_date, end_date
+from tutors t left outer join courses c on t.tutor_id=c.tutor_id
+where t.tutor_id= 1;
+
+select t.tutor_id, t.name as tutor_name, email, 
+		c.course_id, c.name, description, 
+		start_date, end_date
+		from tutors t  join courses c 
+		on t.tutor_id=c.tutor_id
+		where t.tutor_id= 1;
